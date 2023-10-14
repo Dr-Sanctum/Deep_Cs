97c17d98aac1290e799d365471db5cfb86bcca22
anton <Di_oXid@mail.ru>
2023-10-14 17:37:56 +0500

    first commit

---
+++ Приложение/1/Семинар/T2-1/T2-1/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2_1
+{
+
+    internal class Person
+    {
+        public string name { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public Person moteher { get; set; }
+        public Person father { get; set; }
+        public LinkedList<Person> children { get; set; } = new LinkedList<Person>() ;
+
+        public override string? ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ Приложение/1/Семинар/T2-1/T2-1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace T2_1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+           Person a = new Person();
+            Person b = new Person();
+            Person c = new Person();
+            Person d = new Person();
+            a.children.AddLast(b);
+            a.children.AddLast(c);
+            a.children.AddLast(d);
+            foreach (var item in a.children)
+            {
+                Console.WriteLine(item);
+            }
+            
+
+        }
+    }
+}
