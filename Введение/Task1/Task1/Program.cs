b3a0330f668adbef97ce7b5e44ce35ba6040f8fd
anton <Di_oXid@mail.ru>
2023-10-15 18:27:00 +0500

    introduction Task1 complete

---
+++ Введение/Task1/Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Написать программу-калькулятор, вычисляющую выражения вида a + b, a - b, a / b, a * b,
+            //введенные из командной строки, и выводящую результат выполнения на экран.
+            Console.WriteLine("Введите выражение вида a + b, a - b, a / b, a * b");
+            Calc(Console.ReadLine());
+            
+
+        }
+
+        public static void Calc(string expression) {
+            string[] expressionElement = expression.Split(" ");
+            double a = double.Parse(expressionElement[0]);
+            double b = double.Parse(expressionElement[2]);
+
+            switch (expressionElement[1]) { 
+            case "+":
+                    Console.WriteLine( a + b);
+                    break;
+            case "-":
+                    Console.WriteLine(a - b);
+                    break;
+            case "/":
+                    Console.WriteLine(a / b);
+                    break;
+            case "*":
+                    Console.WriteLine(a * b);
+                    break;
+            }
+
+        }
+        }
+    }
