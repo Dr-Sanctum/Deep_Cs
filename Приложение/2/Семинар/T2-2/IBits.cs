4a07036a3a2d3e4a5c4fd200367c0aaca8e12356
anton <Di_oXid@mail.ru>
2023-10-21 14:14:59 +0500

    aplication Task2 complete

---
+++ Приложение/2/Семинар/T2-2/IBits.cs
@@ -0,0 +1,11 @@
+﻿//Спроектируйте интерфейс для класса способного устанавливать и
+//получать значения отдельных бит в заданном числе.
+
+namespace T2_2
+{
+    internal interface IBits
+    {
+        bool GetBit(int i);
+        void SetBit(bool bit, int index);
+    }
+}
---
+++ Приложение/2/Семинар/T2-2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace T2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Bits a = new Bits(534313216L);
+
+
+            for (int i =64; i >= 0; i--)
+            {
+                Console.Write(Convert.ToSByte(a[i]));
+            }
+            Console.WriteLine();
+            a = 12341;
+
+
+            for (int i = 32; i >= 0; i--)
+            {
+                Console.Write(Convert.ToSByte(a[i]));
+            }
+
+            Console.WriteLine();
+            a = 123413457347L;
+
+
+            for (int i = 64; i >= 0; i--)
+            {
+                Console.Write(Convert.ToSByte(a[i]));
+            }
+
+        }
+
+    }
+}
