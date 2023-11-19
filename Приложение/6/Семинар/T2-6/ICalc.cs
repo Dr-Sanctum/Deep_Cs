98657f9bd9860e622357858ae071c3b03ca30363
anton <Di_oXid@mail.ru>
2023-11-19 16:06:08 +0500

    aplication Task6 complete

---
+++ Приложение/6/Семинар/T2-6/ICalc.cs
@@ -0,0 +1,20 @@
+﻿
+namespace T2_6
+{
+    internal interface ICalc
+    {
+        double Result { get; set; }
+
+        double Sum(double x, double y);
+        double Sub(double x, double y);
+        double Divide(double x, double y);
+        double Multy(double x, double y);
+
+        public event EventHandler<EventArgs> CalcChange;
+
+
+    }
+
+
+
+}
---
+++ Приложение/6/Семинар/T2-6/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Доработайте класс калькулятора способным работать как с целочисленными так и с дробными числами. (возможно стоит задействовать перегрузку операций).
+ * заменить Convert.ToDouble на собственный DoubleTryPars и обрабатываем ошибку
+ * проверить что введенное число небыло отрицательное (вывести ошибку Exeption , отловить Catch)
+ * сумма не может быть отрицательной (при делении и вычитании)
+*/
+
+
+
+namespace T2_6
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var calc = new Calc();
+            calc.CalcChange += CalcChangeEvent;
+            calc.Run();
+
+        }
+
+        private static void CalcChangeEvent(object? sender, EventArgs e)
+        {
+            if (sender is Calc)
+            {
+                Console.WriteLine(((Calc)sender).Result);
+            }
+        }
+    }
+}
