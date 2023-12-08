5ac4a146790e1968fd20f33d4411f4d47ce86a1f
anton <Di_oXid@mail.ru>
2023-12-08 21:42:18 +0500

    aplication NetTask2 complete

---
+++ Сетевое приложение/2/Семинар/T3-2/Client/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Chat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new Thread(() =>
+            {
+                using (TcpClient client = new TcpClient())
+                {
+                    client.Connect(IPAddress.Parse("127.0.0.2"), 12345);
+                    var reader = new StreamReader(client.GetStream());
+                    var writer = new StreamWriter(client.GetStream());
+
+
+                    for (int i = 0; i < 100; i++)
+                    {
+                        Thread.Sleep(1500);
+                        var messageClient = new Message() { Text = "Привет сервер", DataTime = DateTime.Now, NickNameFrom = "Клиент " + i, NickNameTo = "Сервер" };
+
+                        writer.WriteLine(messageClient.SerializeMessageToJson());
+                        writer.Flush();
+
+                        var messageFromServer = Message.DeserialiseFromJason(reader.ReadLine());
+
+                        Console.WriteLine(messageFromServer);
+                        
+                        Console.WriteLine("Введите exit для завершения");
+                        string exit = Console.ReadLine();
+                        if (exit != null && exit.ToLower().Equals("exit")){
+                            break;
+                        }
+                        
+                    }
+
+                }
+            }).Start();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Сетевое приложение/2/Семинар/T3-2/Server/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Chat
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            var listener = new TcpListener(IPAddress.Any, 12345);
+            listener.Start();
+
+            using (TcpClient client = listener.AcceptTcpClient())
+            {
+                Console.WriteLine("Connected");
+                var reader = new StreamReader(client.GetStream());
+                var writer = new StreamWriter(client.GetStream());
+                new Thread(() =>
+                {
+                    Console.ReadKey(true);
+                    Console.WriteLine("Сервер завершает свою работу");
+                    flag = false;
+                }).Start();
+
+                while (flag)
+                {
+                    Thread.Sleep(500);
+                    new Thread(() =>
+                    {
+
+
+
+                        var messageFromClient = Message.DeserialiseFromJason(reader.ReadLine());
+                        Console.WriteLine($"{messageFromClient}");
+
+                        var messageServer = new Message() { Text = "Привет клиент, получил твоё сообщение", DataTime = DateTime.Now, NickNameFrom = "Сервер", NickNameTo = messageFromClient.NickNameFrom };
+                        writer.WriteLine(messageServer.SerializeMessageToJson());
+                        writer.Flush();
+                    }).Start();
+                }
+
+            }
+
+        }
+    }
+}
