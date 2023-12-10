575ba3b4b5cbb4b464fda6a6edf9f4af72a66c5d
anton <Di_oXid@mail.ru>
2023-12-10 20:50:01 +0500

    aplication NetTask3 complete

---
+++ Сетевое приложение/3/Семинар/T3-3/T3-3/Server/Message.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+
+namespace Chat
+{
+    public class Message
+    {
+        public string Text { get; set; }
+        public DateTime DataTime { get; set; }
+
+        public string NickNameFrom { get; set; }
+
+        public string NickNameTo { get; set; }
+
+        public string SerializeMessageToJson() => JsonSerializer.Serialize(this);
+
+        public static Message DeserialiseFromJason(string message) => JsonSerializer.Deserialize<Message>(message);
+
+        public override string ToString()
+        {
+            return $"Время: {DataTime}, От кого: {NickNameFrom}, Кому: {NickNameTo} \n {Text}";
+        }
+    }
+
+}
---
+++ Сетевое приложение/3/Семинар/T3-3/T3-3/Server/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+
+namespace Chat
+
+{
+    internal class Program
+    {
+        static private CancellationTokenSource _cts = new CancellationTokenSource();
+        static private CancellationToken _ct = _cts.Token;
+        static void Main(string[] args)
+        {
+           
+            
+            var listener = new TcpListener(IPAddress.Any, 12345);
+            listener.Start();
+
+            using (TcpClient client = listener.AcceptTcpClient())
+            {
+                Console.WriteLine("Connected");
+                var reader = new StreamReader(client.GetStream());
+                var writer = new StreamWriter(client.GetStream());
+                new Task(() =>
+                {
+                    Console.ReadKey(true);
+                    Console.WriteLine("Сервер завершает свою работу");
+                    _cts.Cancel();
+                }).Start();
+
+                while (!_ct.IsCancellationRequested)
+                {
+                    Thread.Sleep(500);
+                    new Task(() =>
+                    {
+                        var messageFromClient = Message.DeserialiseFromJason(reader.ReadLine());
+                        Console.WriteLine($"{messageFromClient}");
+
+                        var messageServer = new Message() { Text = "Привет клиент, получил твоё сообщение", DataTime = DateTime.Now, NickNameFrom = "Сервер", NickNameTo = messageFromClient.NickNameFrom };
+                        writer.WriteLine(messageServer.SerializeMessageToJson());
+                        writer.Flush();
+                    }).Start();
+                }
+
+            }
+
+        }
+    }
+}
