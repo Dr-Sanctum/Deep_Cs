86da0ccd0f3e7041ed74878ef98ba7a2a982f45c
anton <Di_oXid@mail.ru>
2023-12-17 14:53:16 +0500

    aplication NetTask4 complete

---
+++ Сетевое приложение/4/Семинар/T3-4/Client1/ProgramClient1.cs
@@ -0,0 +1,68 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Chat
+{
+    internal class ProgramClient1
+    {
+        private static IPEndPoint ep = new IPEndPoint(IPAddress.Parse("192.168.0.103"), 1234);
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Thread.Sleep(100);
+
+                Console.WriteLine("Введите сообщение, для выхода введите exit");
+                string text = Console.ReadLine();
+                if (text != null && text.ToLower().Equals("exit"))
+                {
+                    break;
+                }
+
+                Console.WriteLine("Введите адресат");
+                string addressee = Console.ReadLine();
+
+                var message = new Message() { Text = text, DataTime = DateTime.Now, NickNameFrom = "Клиент1", NickNameTo = addressee };
+
+               
+
+                var cli = new Task(() => { UdpClientSender(message.SerializeMessageToJson()); });
+                cli.Start();
+                cli.Wait();
+
+                new Task(UdpClientResiver).Start();
+            }
+        }
+        static void UdpClientSender(string message)
+        {
+            using (var client = new UdpClient())
+            {
+                client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                client.Client.Bind(new IPEndPoint(IPAddress.Parse("192.168.0.103"), 3333));
+                client.Connect(ep);
+                byte[] messageByte = Encoding.Default.GetBytes(message);
+                client.Send(messageByte);
+            }
+        }
+
+        static void UdpClientResiver()
+        {
+            var lp = new IPEndPoint(IPAddress.Any, 1);
+            while (true)
+            {
+                using (var client = new UdpClient())
+                {
+                    client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                    client.Client.Bind(new IPEndPoint(IPAddress.Parse("192.168.0.103"), 3333));
+                    var recv = client.Receive(ref lp);
+                    string messageJSON = Encoding.Default.GetString(recv);
+
+                    Message reciveMessage = Message.DeserialiseFromJason(messageJSON);
+
+                    Console.WriteLine(reciveMessage);
+                }
+            }
+        }
+    }
+}
---
+++ Сетевое приложение/4/Семинар/T3-4/Server/ProgramServer.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Data;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace Chat
+
+{
+    internal class ProgramServer
+    {
+        static private CancellationTokenSource _cts = new CancellationTokenSource();
+        static private CancellationToken _ct = _cts.Token;
+        
+
+        static void UdpServerReciver()
+        {
+            //Сервер предварительно добавляет себя в список возможных клиентов
+            var clientList = new Dictionary<string, IPEndPoint>
+            {
+                { "Сервер", new IPEndPoint(IPAddress.Parse("192.168.0.103"), 1234) }
+            };
+
+            using (var client = new UdpClient())
+            {
+                client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                client.Client.Bind(new IPEndPoint(IPAddress.Parse("192.168.0.103"), 1234));
+                var lp = new IPEndPoint(IPAddress.Any, 1111);
+                while (!_ct.IsCancellationRequested)
+                {
+                    var recv = client.Receive(ref lp);
+                    
+                    string messageJSON = Encoding.Default.GetString(recv);
+
+                    Message reciveMessage = Message.DeserialiseFromJason(messageJSON);
+
+                    
+
+                    Console.WriteLine(reciveMessage);
+
+                    if (reciveMessage.NickNameTo.Equals("Сервер"))
+                    {
+                        switch (reciveMessage.Text.ToLower())
+                        {
+                            case "register":
+                                clientList.TryAdd(reciveMessage.NickNameFrom, lp);
+                                break;
+                            case "delete":
+                                clientList.Remove(reciveMessage.NickNameFrom);
+                                break;
+                            case "clients":
+                                StringBuilder stringBuilder = new StringBuilder();  
+                                stringBuilder.Append("Список адресатов: ");
+                                foreach (var item in clientList.Keys)
+                                {
+                                    stringBuilder.Append($"{item} ");
+                                }
+                                var messageByteServer = MakeMessageToSend(stringBuilder.ToString(), DateTime.Now, "Сервер", reciveMessage.NickNameFrom);
+
+                                new Task(() => { UdpClientSender(clientList[reciveMessage.NickNameFrom], messageByteServer); }).Start();
+                                break;
+                            default:
+                                break;
+                        }
+                        continue;
+                    }
+                    if (!clientList.ContainsKey(reciveMessage.NickNameFrom))
+                    {
+                        var messageByteServer = MakeMessageToSend("Вы не зарегистрированы", DateTime.Now, "Сервер", reciveMessage.NickNameFrom);
+
+                        new Task(() => { UdpClientSender(lp, messageByteServer); }).Start();
+                        continue;
+                    }
+
+                    if (reciveMessage.NickNameTo == string.Empty)
+                    {
+                        foreach (var nameClient in clientList.Keys)
+                        {
+                            if (nameClient.Equals("Сервер"))
+                            {
+                                continue;
+                            }
+                            new Task(() => { UdpClientSender(clientList[nameClient], recv); }).Start();
+                        }
+                        continue;
+                    }
+
+                    if (!clientList.ContainsKey(reciveMessage.NickNameTo))
+                    {
+                        var messageByteServer = MakeMessageToSend("Клиента с таким именем не зарегистрировано", DateTime.Now, "Сервер", reciveMessage.NickNameFrom);
+
+                        new Task(() => { UdpClientSender(clientList[reciveMessage.NickNameFrom], messageByteServer); }).Start();
+                        continue;
+                    }
+                    new Task(() => { UdpClientSender(clientList[reciveMessage.NickNameTo], recv); }).Start();
+                    
+                }
+               
+            }
+        }
+
+
+        static byte[] MakeMessageToSend(string text, DateTime dataTime, string nickNameFrom, string nickNameTo)
+        {
+            var messageServer = new Message() { Text = text, DataTime = dataTime, NickNameFrom = nickNameFrom, NickNameTo =  nickNameTo};
+            return Encoding.Default.GetBytes(messageServer.SerializeMessageToJson());
+        }
+
+        static void UdpClientSender(IPEndPoint client, byte[] byteMessage)
+        {
+            using (var newClient = new UdpClient())
+            {
+                newClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                newClient.Client.Bind(new IPEndPoint(IPAddress.Parse("192.168.0.103"), 1234));
+                newClient.Connect(client);
+                newClient.Send(byteMessage);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            
+            new Task(() =>
+            {
+                Console.ReadKey(true);
+                Console.WriteLine("Сервер завершает свою работу.");
+                _cts.Cancel();
+            
+            }).Start();
+            
+            IPEndPoint IPClient = new IPEndPoint(IPAddress.Any, 1234);
+            var serverReciver = new Task(UdpServerReciver) ;
+            serverReciver.Start();
+            
+            serverReciver.Wait();
+            
+
+        }
+    }
+}
